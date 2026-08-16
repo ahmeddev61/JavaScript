d74ccc95cf51291cf629b05c16c29b2540ee21d2
ahmeddev61 <ahmeddani.dev@gmail.com>
2026-08-15 21:51:44 -0700

    java script started

---
+++ class_1/intro/index.js
@@ -0,0 +1,11 @@
+// calc = prompt("enter number");
+// if (calc >= 18) {
+//   console.log("you are eligible");
+// } else {
+//   console.log("you are not eligible");
+// }
+let a = Number(prompt("enter number 1"));
+let b = Number(prompt("enter number 2"));
+const add = a + b;
+console.log(add);
+alert("Done");
